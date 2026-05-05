076eab1abeee557098ef3bf1bd9815f6b8cd1d34
ZiClaud <claudiodimaio@tutamail.com>
2026-05-05 11:16:39 +0200

    Created tile with decoration

---
+++ classes/DecorationTile.gd
@@ -0,0 +1,9 @@
+class_name DecorationTile
+extends BaseTile
+
+@onready var decoration: Decoration = %Decorations
+
+
+func _ready() -> void:
+	decoration.show_decorations()
+	self.add_child(decoration)
