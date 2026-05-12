8083b74d1aefa567c53873c5c63bac594dc16645
ZiClaud <claudiodimaio@tutamail.com>
2026-05-12 09:47:46 +0200

    continue

--- classes/generation/BaseGeneration.gd
+++ classes/generation/BaseGeneration.gd
@@ -7,14 +7,6 @@ var generation_mode: GenerationMode # TODO: Change in settings UI
 #@export var all_tiles_ps: Array[PackedScene] = []
 #@export var tiles_weight: Array[int] = []
 
-const GRASS_TILE_SCENE: PackedScene = preload("res://tiles/grass_tile.tscn")
-const WATER_TILE_SCENE: PackedScene = preload("res://tiles/water_tile.tscn")
-const DECORATION_TILE_SCENE: PackedScene = preload("res://tiles/decoration_tile.tscn")
-const ROADS_TILES_SCENE: Array[PackedScene] = [preload("res://tiles/road_a_tile.tscn"), preload("res://tiles/road_b_tile.tscn"), preload("res://tiles/road_c_tile.tscn")]
-const COASTS_TILES_SCENE: Array[PackedScene] = [preload("res://tiles/coast_a_tile.tscn"), preload("res://tiles/coast_b_tile.tscn"), preload("res://tiles/coast_c_tile.tscn"), preload("res://tiles/coast_d_tile.tscn"), preload("res://tiles/coast_e_tile.tscn")]
-const RIVERS_TILES_SCENE: Array[PackedScene] = [preload("res://tiles/river_a_tile.tscn"), preload("res://tiles/river_a2_tile.tscn"), preload("res://tiles/river_b_tile.tscn"), preload("res://tiles/river_c_tile.tscn")]
-const ROADS_RIVERS_TILES_SCENE: Array[PackedScene] = [preload("res://tiles/road_river_a_tile.tscn"), preload("res://tiles/road_river_b_tile.tscn")]
-
 var all_tiles_ps: Array[PackedScene] = []
 
 @export var world_size: int = 16
